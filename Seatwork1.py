12f3d879368c8db581966961b9f174b8a6531803
unknown <seanharveyofiangga@gmail.com>
2022-10-22 16:34:18 +0800

    Created pattern for E

--- Seatwork1.py
+++ Seatwork1.py
@@ -18,4 +18,10 @@ def myname():
             if ((row==0 or row==3 or row==6) and (col>0 and col<4)) or (col==0 and (row>0 and row<3)) or (col==4 and (row>3 and row<6)):
                 print_S[row][col] = "*"
 
+    # E
+    print_E = [[" " for i in range(7)] for j in range (7)]
+    for row in range(7):
+        for col in range(5):
+            if col==0 or ((row==0 or row==3 or row==6) and (col>0)):
+                print_E[row][col] = "*"
 
